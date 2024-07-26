0d2440144a472fd6ecb4e765b12ca122a365a2a3
Mahmoud Hafez Eltarqi <xbrymo01@gmail.com>
2024-07-26 15:39:07 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
+import 'profile.dart';
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MainApp());
 }
 
@@ -10,10 +15,107 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+      home: SplashScreen(),
+    
+    );
+  }
+}
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  SplashScreenState createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  var _visible = true;
+
+  AnimationController? animationController;
+  Animation<double>? animation;
+
+  startTime() async {
+    var duration = const Duration(seconds: 4);
+    return Timer(duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+      return const ProfilePage();
+    }));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+        vsync: this, duration: const Duration(seconds: 2));
+    animation =
+        CurvedAnimation(parent: animationController!, curve: Curves.easeOut);
+
+    animation!.addListener(() => setState(() {}));
+    animationController!.forward();
+
+    setState(() {
+      _visible = !_visible;
+    });
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo.shade400,
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          const Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.only(bottom: 30.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(top: 4.0),
+                        child: Text(
+                          'Health',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25.0),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 4.0, left: 5.0),
+                        child: Text(
+                          'Care',
+                          style: TextStyle(
+                              color: Colors.red,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25.0),
+                        ),
+                      ),
+                    ],
+                  ))
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                'assets/logo.png',
+                width: animation!.value * 250,
+                height: animation!.value * 250,
+              ),
+              const Text("Healthy Wealthy", style: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold, ),),
+            ],
+          ),
+        ],
       ),
     );
   }
